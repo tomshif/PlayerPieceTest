cce2a083642e0dc86ad7938f6510fe58324f1f77
Tom Shiflet <tom.shiflet@mylcs.com>
2019-12-03 07:10:20 -0600

    Go

--- PlayerPieceTest/GameScene.swift
+++ PlayerPieceTest/GameScene.swift
@@ -11,55 +11,40 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+
+    let body=SKSpriteNode(imageNamed: "body")
+    let head=SKSpriteNode(imageNamed: "head")
+    let arms=SKSpriteNode(imageNamed: "arms")
+    
+    var bScale:CGFloat=2.0
+    
     
     override func didMove(to view: SKView) {
+    
+        body.setScale(bScale)
+        addChild(body)
+        body.addChild(arms)
+        body.addChild(head)
+        head.zPosition=1
+        arms.zPosition=1
+        body.colorBlendFactor=1.0
+        arms.colorBlendFactor=1.0
+        head.colorBlendFactor=1.0
+
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -76,10 +61,24 @@ class GameScene: SKScene {
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+
+        case 27:
+            if bScale > 1.0
+            {
+                bScale -= 0.5
+                body.setScale(bScale)
             }
+            
+        case 24:
+            if bScale < 16
+            {
+                bScale += 0.5
+                body.setScale(bScale)
+            }
+        case 49:
+            body.color=NSColor(calibratedRed: random(min: 0, max: 1), green: random(min: 0, max: 1), blue: random(min: 0, max: 1), alpha: 1.0)
+            arms.color=NSColor(calibratedRed: random(min: 0, max: 1), green: random(min: 0, max: 1), blue: random(min: 0, max: 1), alpha: 1.0)
+            head.color=NSColor(calibratedRed: random(min: 0, max: 1), green: random(min: 0, max: 1), blue: random(min: 0, max: 1), alpha: 1.0)
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
